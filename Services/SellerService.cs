7313b8f8a25c1a46a770d5e7f842054c3521f80e
JocemarHpereira <jocemarpere@gmail.com>
2022-07-24 20:55:30 -0300

    SellerModelFormView

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -20,8 +20,7 @@ namespace SalesWebMVC.Services
             return _context.Seller.ToList();
         }
 
-        public void Insert(Seller obj) {
-            obj.Department = _context.Department.First();
+        public void Insert(Seller obj) {            
             _context.Add(obj);
             _context.SaveChanges();
         }
