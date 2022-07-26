959be5c89ca4c997203ce0959c968643e98380be
JocemarHpereira <jocemarpere@gmail.com>
2022-07-25 21:42:47 -0300

    Implementing grouping serach

--- Services/SalesRecordService.cs
+++ Services/SalesRecordService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
+using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Services
 {
@@ -31,6 +32,23 @@ namespace SalesWebMVC.Services
                 .Include(x => x.Seller)
                 .Include(x => x.Seller.Department)
                 .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+        public async Task<List<IGrouping<Department, SalesRecord>>>FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate) {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x=> x.Seller.Department)
                 .ToListAsync();
         }
     }
